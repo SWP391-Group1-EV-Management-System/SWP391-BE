d271e3da910c7b366a05df7c5294a181eddd2b6c
Ho-VietKhanh <kho20232@gmail.com>
2025-09-20 14:32:23 +0700

    Create main constructor

--- src/main/java/charging_manage_be/Main.java
+++ src/main/java/charging_manage_be/Main.java
@@ -4,9 +4,6 @@ package charging_manage_be;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+
     }
 }
