40961bbd07a4c62c2d8244ba7138da1498a3efa0
Nguyen Hoang Bao <hoangbaon924@gmail.com>
2025-10-16 16:23:31 +0700

    update cookie save token

--- src/main/java/charging_manage_be/security/JwtFilter.java
+++ src/main/java/charging_manage_be/security/JwtFilter.java
@@ -3,6 +3,7 @@ package charging_manage_be.security;
 import charging_manage_be.services.UserToken.CustomerDetailService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,15 +28,23 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
 
-        final String authHeader = request.getHeader("Authorization"); // Lấy header sau khi đăng nhập thành công
+        //final String authHeader = request.getHeader("Authorization"); // Lấy header sau khi đăng nhập thành công
         String email = null;
         String jwt = null;
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) { // Kiểm tra xem header có hợp lệ không
-            jwt = authHeader.substring(7);
-            email = jwtUtil.extractUsername(jwt); // Lấy email
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("jwt")) {
+                    jwt = cookie.getValue();  // Lấy token thuần từ cookie
+                    break;
+                }
+            }
         }
 
+        if (jwt != null) {
+            email = jwtUtil.extractUsername(jwt);
+        }
         if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) { // kiểm tra email và request trống
             //email hợp lệ tiếp tục kiểm tra
             UserDetails userDetails = detailsService.loadUserByUsername(email);
